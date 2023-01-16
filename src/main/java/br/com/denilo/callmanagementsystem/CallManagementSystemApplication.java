18cba015bfdd8f2b7ba062497934e0962190ce57
Denilo Diniz <denilo_diniz@outlook.com>
2023-01-16 19:59:56 -0300

    Created project

---
+++ src/main/java/br/com/denilo/callmanagementsystem/CallManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.denilo.callmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/denilo/callmanagementsystem/CallManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.denilo.callmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
